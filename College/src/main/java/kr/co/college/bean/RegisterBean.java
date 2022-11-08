c799f7b0b96c75105fc5c6797d29e0bcc71bfb3e
lovelyroaflower <lovelyroaflower@gmail.com>
2022-11-08 13:29:54 +0900

    2022/11/08

---
+++ College/src/main/java/kr/co/college/bean/RegisterBean.java
@@ -0,0 +1,54 @@
+package kr.co.college.bean;
+
+public class RegisterBean {
+	private int regStdNo;
+	private int regLecNo;
+	private int regMidScore;
+	private int regFinalScore;
+	private int regTotalScore;
+	private String regGrade;
+	
+	
+	public int getRegStdNo() {
+		return regStdNo;
+	}
+	public void setRegStdNo(int regStdNo) {
+		this.regStdNo = regStdNo;
+	}
+	public int getRegLecNo() {
+		return regLecNo;
+	}
+	public void setRegLecNo(int regLecNo) {
+		this.regLecNo = regLecNo;
+	}
+	public int getRegMidScore() {
+		return regMidScore;
+	}
+	public void setRegMidScore(int regMidScore) {
+		this.regMidScore = regMidScore;
+	}
+	public int getRegFinalScore() {
+		return regFinalScore;
+	}
+	public void setRegFinalScore(int regFinalScore) {
+		this.regFinalScore = regFinalScore;
+	}
+	public int getRegTotalScore() {
+		return regTotalScore;
+	}
+	public void setRegTotalScore(int regTotalScore) {
+		this.regTotalScore = regTotalScore;
+	}
+	public String getRegGrade() {
+		return regGrade;
+	}
+	public void setRegGrade(String regGrade) {
+		this.regGrade = regGrade;
+	}
+	
+	
+	
+	
+	
+	
+}
